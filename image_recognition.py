c9335f88ee811186afd835d1371162da577837d9
Michael Duncan <mwduncan2018@gmail.com>
2019-12-15 16:14:44 -0500

    updates

--- image_recognition.py
+++ image_recognition.py
@@ -1,65 +1,65 @@
+# install OpenCV like this --> pip install opencv-python
 import os
 import sys
 import cv2
 import numpy as np
 
-# Template Image = the template you are searching for
-template_image = r''
-# Screenshot Image = the image you are searching
-screenshot_image = r''
+# Template Image = this is the template you are searching for
+template_image = r'C:\dev\Python\python_opencv\images\template_ocean_city.jpg'
+
+# Screenshot Image = this is the image you are searching
+screenshot_image = r'C:\dev\Python\python_opencv\images\screenshot_maryland_map.jpg'
+
+# Threshold for template matching
+threshold = 0.97
 
 class ImageSolution:
 
-	def __init__(self, template_image, screenshot_image):
-		print(template_image)
-		print(screenshot_image)
+	def __init__(self, template_image, screenshot_image, threshold):
+	
 		self._template_image = template_image
 		self._screenshot_image = screenshot_image
+		self._threshold = threshold
+
+		self._template_width = None
+		self._template_height = None
 		self._screen = None
 		self._loc = None
+
 		
 	def calculate_template_match_coordinates(self):
+	
 		self._screen = cv2.imread(self._screenshot_image)
 		gray_screen = cv2.cvtColor(self._screen, cv2.COLOR_BGR2GRAY)
+
 		template = cv2.imread(template_image, 0)
-		w, h = template.shape[::-1]
-		print("Template shape:")
-		print("w = " + str(w))
-		print("h = " + str(h))
-		print("")
-		result = cv2.matchTemplate(gray_screen, template, cv2.TM_CCOEFF_NORMED)
-		print("result type is .... " + str(type(result)))
-		print("")
-		print("Result:")
-		print(result)
-		print("")
-		threshold = 0.97
-		self._loc = np.where(result >= threshold)
-		print("")
-		print("loc:")
-		print(self._loc)
+		self._template_width, self._template_height = template.shape[::-1]
 		
-	def write_results_to_file(self, write_file):
-		"""Writes the results to a file so Java can consume.
-		"""
-		f = open(write_file, 'w')
-		num_matches = 0
-		for pt in zip(*self._loc[::-1]):
-			num_matches = num_matches + 1
-			f.write(str(pt) + '\n')
-			print("Match found...")
-			print("   Upperleft  = " + str(pt))
-			print("   Upperright = " + str( (pt[0] + w, pt[1] + h) ))
-			cv2.rectangle(screen, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-		if num_matches == 0:
-			f.write('None')
-		print("Number of Matches = " + str(num_matches))
-		f.close()
+		# There are several algorithm options. This example shows 'TM_CCOEFF_NORMED'. Check out the OpenCV documentation for the others.
+		result = cv2.matchTemplate(gray_screen, template, cv2.TM_CCOEFF_NORMED)
+
+		# A threshold around 97% works best.
+		self._loc = np.where(result >= self._threshold)
+
 
 	def display_results(self):
 		"""Display the results in a window with a red rectangle around the matches.
 		"""
-		cv2.imshow("screen", self._screen)
+		print("\nScreenshot Path:")
+		print("  " + str(screenshot_image))
+		print("\nTemplate Path:")
+		print("  " + str(template_image))
+		print("\nTemplate Dimensions:")
+		print("  width = " + str(self._template_width))
+		print("  height = " + str(self._template_height))
+
+		print("\n\n=== Matches ===")
+		for pt in zip(*self._loc[::-1]):
+			print("\n  Match found:")
+			print("    Upper-left match coordinate = " + str(pt))
+			print("    Lower-right match coordinate = " + str( (pt[0] + self._template_width, pt[1] + self._template_height) ))
+			cv2.rectangle(self._screen, pt, (pt[0] + self._template_width, pt[1] + self._template_height), (0, 0, 255), 2)
+		cv2.imshow("Python OpenCV - Template Matching Demo", self._screen)
 		cv2.waitKey(0)
 		cv2.destroyAllWindows()
 
@@ -68,14 +68,9 @@ if __name__ == '__main__':
 	# Uncomment if running from the command line
 	#template_image = sys.argv[1]
 	#screenshot_image = sys.argv[2]
-	#reult_path = sys.argv[3]
 
-	sol = ImageSolution(template_image, screenshot_image)
-	sol.calculate_template_match_coordinates()
-	#sol.write_results_to_file(result_path)
-	sol.display_results()
+	solution = ImageSolution(template_image, screenshot_image, threshold)
+	solution.calculate_template_match_coordinates()
+	solution.display_results()
 
-	print('COMPLETED... image_solution.py :)')
-	#w, h = template.shape[::-1]
-	#print(str(w))
-	#print(str(h))
+	print('\n\nCOMPLETED... image_recognition.py :)\n')
